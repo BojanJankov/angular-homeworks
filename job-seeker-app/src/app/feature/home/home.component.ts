d95a9bcf6fd5162b73daa23f85d7920b5f072215
Bojan Jankov <jankovbojan4@gmail.com>
2024-08-23 20:32:18 +0200

    added and finish home page

--- job-seeker-app/src/app/feature/home/home.component.ts
+++ job-seeker-app/src/app/feature/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,10 @@ import { ButtonComponent } from '../../shared/components/button/button.component
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private router = inject(Router);
+
+  onNaviagteClick() {
+    this.router.navigate(['jobs']);
+  }
+}
