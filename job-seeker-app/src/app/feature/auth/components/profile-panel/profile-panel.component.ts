22c1ea5ab13be7d4abe5b4bedcc72419c9429ce0
Bojan Jankov <jankovbojan4@gmail.com>
2024-08-25 12:55:08 +0200

    added profile panel page

--- job-seeker-app/src/app/feature/auth/components/profile-panel/profile-panel.component.ts
+++ job-seeker-app/src/app/feature/auth/components/profile-panel/profile-panel.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../models/auth.model';
+import { userMock } from '../../users.mock';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile-panel',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './profile-panel.component.html',
-  styleUrl: './profile-panel.component.scss'
+  styleUrl: './profile-panel.component.scss',
 })
 export class ProfilePanelComponent {
-
+  user = signal<User>(userMock);
 }
---
+++ job-seeker-app/src/app/feature/auth/models/auth.model.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  gender: string;
+  email: string;
+  profesion: string;
+  about: string;
+  education: string;
+}
